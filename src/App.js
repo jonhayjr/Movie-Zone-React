5bc738f4a82525bfd0816a4eb996de2ab9f52203
jonhayjr <jonhayjr@gmail.com>
2022-01-27 08:04:26 -0800

    Added logic to get movies from API

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
 import './App.css';
 
 /*Import Components*/ 
 import Header from './components/Header/Header';
 
+const apiKey = process.env.REACT_APP_MOVIES_API_KEY;
+
 const App = () => {
+
+  const [movies, setMovies] = useState([]);
+  const [favoriteMovies, setFavoriteMovies] = useState([]);
+  const [searchValue, setSearchValue] = useState('');
+
+  useEffect(() => {
+    const defaultSearch = 'marvel';
+    const URL = `https://www.omdbapi.com/?apiKey=${apiKey}&type=movie&s=${defaultSearch}`;
+    axios.get(URL)
+          .then(
+            res => {
+              setMovies(res.data.Search);
+            })
+            .catch(err => {
+              console.log(err);
+            })
+  }, [])
+
   return (
     <div className="App">
       <Header/>
+
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => {
   return (
   <header className="text-center p-5 bg-black text-white">
-    <h1 class="shadow-sm bg-dark rounded p-2 w-25 mx-auto">Movie<span className="text-danger">Zone</span></h1>
+    <h1 className="shadow-sm bg-dark rounded p-2 w-75 mx-auto">Movie<span className="text-danger">Zone</span></h1>
   </header>
   )
 };
