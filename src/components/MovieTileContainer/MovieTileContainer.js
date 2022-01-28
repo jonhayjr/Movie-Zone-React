b4327f2883bc2dc35b3cd31cd2aa8a8bc28fc9ee
jonhayjr <jonhayjr@gmail.com>
2022-01-27 20:23:58 -0800

    Created MovieTileContainer

---
+++ src/components/MovieTileContainer/MovieTileContainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MovieTileContainer = ({children}) => {
+  return (
+    <div className="grid-container">
+        {children}
+    </div>
+  )
+};
+
+export default MovieTileContainer;
