dec13dc65d4d391f84c86a367ce0e80b170208dc
jonhayjr <jonhayjr@gmail.com>
2022-01-30 12:10:31 -0800

    Fixed typo in AboutView component

--- src/components/AboutView/AboutView.tsx
+++ src/components/AboutView/AboutView.tsx
@@ -5,7 +5,7 @@ const AboutView = () => {
   return (
     <div className="mt-5 text-center w-50 mx-auto border border-white bg-dark text-white p-5 border-rounded">
         <h2>About</h2>
-        <p>This Angular app was created by Jon Hay in 2022.  The app allows use to search for movies and then add those movies to their favorites.</p>
+        <p>This Angular app was created by Jon Hay in 2022.  The app allows users to search for movies and then add those movies to their favorites.</p>
         <Link to="/" className="text-white">Go Home</Link>
     </div>
   )
